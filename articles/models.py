94e8797e61bafcda5e35929233b69993c9ce5c66
banggeut01 <genie121110@gmail.com>
2019-09-16 10:43:26 +0900

    12 | Create Comment(1:N) Model

--- articles/models.py
+++ articles/models.py
@@ -25,3 +25,7 @@ class Article(models.Model):
 #                : DB 설계도
 # migrate : migration 파일 DB 반영
 
+class Comment(models.Model):
+    content = models.CharField(max_length=140)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
